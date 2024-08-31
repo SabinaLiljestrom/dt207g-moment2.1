d3215177844736709277818aec3f3b7673bdfc7c
Sabina Liljeström <sabina.liljestrom@hotmail.com>
2024-08-31 20:26:24 +0200

    skapat databas och börjat på server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+/**
+ * Arbetserfarenheter
+ * Av Sabina Liljeström
+ */
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const cors = require('cors');
